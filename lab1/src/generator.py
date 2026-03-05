e1c771c576a6c1e26409f5e682900fb559a1588d
artems <whard8y@gmail.com>
2026-03-05 10:12:12 +0300

    Remove space in chars

--- lab1/src/generator.py
+++ lab1/src/generator.py
@@ -6,7 +6,7 @@ def main():
     num_records = int(sys.argv[1]) if len(sys.argv) > 1 else 10000
     out_file = sys.argv[2] if len(sys.argv) > 2 else "input.txt"
     
-    alphabet = string.ascii_letters + string.digits + " _-.,:;()[]{}<>@#%&+=?/\\|~^"
+    alphabet = string.ascii_letters + string.digits + "_-.,:;()[]{}<>@#%&+=?/\\|~^"
     
     with open(out_file, 'w', encoding='utf-8') as f:
         for _ in range(num_records):
