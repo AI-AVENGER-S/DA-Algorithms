e3a55da9f34d583a2c760a803f0fe130f6d782a5
artems <whard8y@gmail.com>
2026-03-05 10:11:35 +0300

    Remove try except

--- lab1/src/checker.py
+++ lab1/src/checker.py
@@ -10,40 +10,36 @@ def main():
     line_no = 0
     errors = 0
 
-    try:
-        with open(path, "r", encoding="utf-8") as f:
-            for raw_line in f:
-                line_no += 1
-                line = raw_line.rstrip('\r\n')
-                
-                if not line: continue
-                
-                parts = line.split("\t")
-                if len(parts) < 2:
-                    print(f"Error at line {line_no}: No tab separator. (Line content: '{line}')")
+    with open(path, "r", encoding="utf-8") as f:
+        for raw_line in f:
+            line_no += 1
+            line = raw_line.rstrip('\r\n')
+            
+            if not line: continue
+            
+            parts = line.split("\t")
+            if len(parts) < 2:
+                print(f"Error at line {line_no}: No tab separator. (Line content: '{line}')")
+                errors += 1
+                continue
+            
+            try:
+                key = int(parts[0])
+                if key < prev_key:
+                    print(f"Sort Error at line {line_no}: Key {key} < Previous {prev_key}")
                     errors += 1
-                    continue
-                
-                try:
-                    key = int(parts[0])
-                    if key < prev_key:
-                        print(f"Sort Error at line {line_no}: Key {key} < Previous {prev_key}")
-                        errors += 1
-                    prev_key = key
-                except ValueError:
-                    print(f"Error at line {line_no}: Key is not an integer.")
-                    errors += 1
+                prev_key = key
+            except ValueError:
+                print(f"Error at line {line_no}: Key is not an integer.")
+                errors += 1
 
-        if errors == 0:
-            print(f"Check OK: {line_no} lines scanned, file is sorted.")
-            sys.exit(0)
-        else:
-            print(f"Check Failed: {errors} errors found.")
-            sys.exit(1)
-
-    except FileNotFoundError:
-        print(f"Error: File {path} not found.")
+    if errors == 0:
+        print(f"Check OK: {line_no} lines scanned, file is sorted.")
+        sys.exit(0)
+    else:
+        print(f"Check Failed: {errors} errors found.")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
